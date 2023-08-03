6840f2e415b7b7403529f89a84773f9e59d78407
Nozimjon Usmonaliyev <usminaliyevnozimjon@gmail.com>
2023-08-03 09:11:47 +0500

    bug fixed

--- ArmadaMotors.Service/DTOs/Products/MatorEngineForResultDto.cs
+++ ArmadaMotors.Service/DTOs/Products/MatorEngineForResultDto.cs
@@ -2,6 +2,7 @@
 
 public class MatorEngineForResultDto
 {
+	public long  Id { get; set; }
 	public string Name { get; set; }
 	public Decimal Price { get; set; }
 	public long ProductId { get; set; }
