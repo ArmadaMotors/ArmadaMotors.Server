15c08470550a82f5063d5c950117e2a3fd4a4232
mtukhtaboev <75162831+MrTukhtaboev@users.noreply.github.com>
2023-08-07 01:06:13 +0500

    test route changed

--- ArmadaMotors.Api/Controllers/UsersController.cs
+++ ArmadaMotors.Api/Controllers/UsersController.cs
@@ -45,7 +45,7 @@ namespace ArmadaMotors.Api.Controllers
         public async ValueTask<IActionResult> UpdateRoleAsync([FromRoute(Name = "Id")] long id, UserRole role)
             => Ok(await _userService.ModifyRoleAsync(id, role));
 
-        [HttpGet("Test"), AllowAnonymous]
+        [HttpGet("Test2"), AllowAnonymous]
         public IActionResult GetTestResult()
             => Ok("Hello World!");
     }
